e48c851fb2439eeed38ee0d714f057aa4f1db6a2
Yonas <yonas.yishac@gmail.com>
2026-04-21 11:23:48 +0300

    chore(assets): replace partner logos
    Use the new logo assets under src/logo/newlogo and remove the previous placeholder logo images.

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -6,19 +6,14 @@ export type Partner = {
   logo: StaticImageData
 }
 
-import logo1 from "@/logo/1.png"
-import logo2 from "@/logo/2.png"
-import logo3 from "@/logo/3.png"
-import logo4 from "@/logo/4.png"
-import logo5 from "@/logo/5.png"
-import logo6 from "@/logo/6.png"
+import newLogo from "@/logo/newlogo/Group.png"
 
 export const partners: Partner[] = [
-  { id: "partner_1", name: "Partner 1", logo: logo1 },
-  { id: "partner_2", name: "Partner 2", logo: logo2 },
-  { id: "partner_3", name: "Partner 3", logo: logo3 },
-  { id: "partner_4", name: "Partner 4", logo: logo4 },
-  { id: "partner_5", name: "Partner 5", logo: logo5 },
-  { id: "partner_6", name: "Partner 6", logo: logo6 },
+  { id: "partner_1", name: "Partner 1", logo: newLogo },
+  { id: "partner_2", name: "Partner 2", logo: newLogo },
+  { id: "partner_3", name: "Partner 3", logo: newLogo },
+  { id: "partner_4", name: "Partner 4", logo: newLogo },
+  { id: "partner_5", name: "Partner 5", logo: newLogo },
+  { id: "partner_6", name: "Partner 6", logo: newLogo },
 ]
 
